294d065b3264cd8110f96c9f9469fa9b8ed68682
diegolenci <lencidiego0@gmail.com>
2025-03-17 17:15:31 +0100

    Add: layout con home page e movie page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+export default function HomePage(){
+    return(
+        <>
+             <h1 className="text-primary">Movie</h1>
+             <div className="row row-cols-3">
+                {/* qui andranno tutti i film */}
+             </div>
+        </>
+    )
+}
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,7 @@
+export default function MoviePage(){
+    return(
+        <>
+            <h1>Singolo film</h1>
+        </>
+    )
+}
