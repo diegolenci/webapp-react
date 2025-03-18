4ff57786bd7da62786d09b07631e2c9f603a158a
diegolenci <lencidiego0@gmail.com>
2025-03-18 15:39:01 +0100

    Add: funzione fetch per i film e chiamata axios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
 import MovieCard from "../components/MovieCard";
 
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export default function HomePage(){
+
+    const [movies, setMovies] = useState([])
+
+    //funzione fetch per i film
+    const fetchMovies = () => {
+        console.log('Fetching movies...')
+
+        axios.get('http://localhost:3000/movies')
+        .then( res => { setMovies(res.data) } )
+        .catch( error => { console.log(error) } )
+    }
+
     return(
         <>
              <h1 className="text-primary">Movie</h1>
