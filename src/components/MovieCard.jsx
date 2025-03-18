e4145852be87bc55295d49cf82e9adfef99782d4
diegolenci <lencidiego0@gmail.com>
2025-03-18 16:39:12 +0100

    Add: chiamata per ottenere la lista dei film

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard(){
+export default function MovieCard({movie}){
+
+    const { id, title, director, abstract, release_year, image } = movie
+    
     return(
         <>
             <div className="card mb-4">
                 <div className="card-body">
-                    <h5 className="card-title">Titolo film</h5>
-                    <span>
-                        Author
-                    </span>
+                    <h3 className="card-title">{title}</h3>
+                    <h5>
+                        {director}
+                    </h5>
+                    <h6>
+                        {release_year}
+                    </h6>
                     <p>
-                        Lorem ipsum dolor, sit amet consectetur audispicing elit
+                        {abstract}
                     </p>
-                    <Link to={'movies/2'}>Watch more</Link>
+                    <Link to={`movies/${id}`}>Watch more</Link>
                 </div>
             </div>
         </>
