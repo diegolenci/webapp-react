3d4c3d9f22dc42595836d4f9c4483ac51428e500
diegolenci <lencidiego0@gmail.com>
2025-03-18 15:48:01 +0100

    Add: funzione renderMovies

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,12 +15,26 @@ export default function HomePage(){
         .then( res => { setMovies(res.data) } )
         .catch( error => { console.log(error) } )
     }
+    //funzione che richiama le card dei film poi usata sotto nel return
+    const renderMovies = () => {
+        return(
+            movies.map( (movie) => {
+                return(
+                    <div className="col" key={movie.id}>
+                        <MovieCard movie={movie}/>
+                    </div>
+                )
+            })
+        )
+    }
+
+    useEffect( fetchMovies, [] )
 
     return(
         <>
              <h1 className="text-primary">Movie</h1>
              <div className="row row-cols-3">
-                <MovieCard/>
+                {renderMovies()}
                 
              </div>
         </>
